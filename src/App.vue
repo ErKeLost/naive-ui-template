ecd5b339774aa69d4a5c66908845c556c15a5649
erkelost <1256029807@qq.com>
2022-04-03 19:20:44 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,7 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 const info = () => {
-  $message.warning('This is a warning message')
-}
-const dialog = () => {
-  $dialog.warning({
-    title: '123123132',
-    content: '你确定？',
-    positiveText: '确定',
-    negativeText: '不确定',
-    onPositiveClick: () => {
-      $message.success('确定')
-    },
-    onNegativeClick: () => {
-      $message.error('不确定')
-    }
-  })
+  window.$message!.warning('This is a warning message')
 }
 </script>
 
@@ -25,7 +11,6 @@ const dialog = () => {
     <template #adny>
       <n-space>
         <n-button @click="info">Default</n-button>
-        <n-button @click="dialog" type="tertiary"> Tertiary </n-button>
         <n-button type="primary"> Primary </n-button>
         <n-button type="info"> Info </n-button>
         <n-button type="success"> Success </n-button>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+const message = window['$message']
+const app = createApp(App)
+// 注册全局方法，如：app.config.globalProperties.$message = message
+app.config.globalProperties.$message = message
+// setupGlobalMethods(app)
+app.mount('#app')
