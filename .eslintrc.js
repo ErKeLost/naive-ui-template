93e3f07b4dd033b39a275bcc60c9fe639d06173c
erkelost <1256029807@qq.com>
2022-04-03 15:21:30 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  globals: {
+    defineEmits: true,
+    document: true,
+    localStorage: true,
+    GLOBAL_VAR: true,
+    window: true,
+    defineProps: true,
+    defineExpose: true
+  },
+  extends: [
+    './.eslintrc-auto-import.json',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:prettier/recommended' // 添加 prettier 插件s
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: ['vue', '@typescript-eslint', 'import', 'prettier'],
+  rules: {
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'vue/multi-word-component-names': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'vue/no-mutating-props': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/ban-types': 'off'
+  }
+}
---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <n-space>
+    <n-button>Default</n-button>
+    <n-button type="tertiary"> Tertiary </n-button>
+    <n-button type="primary"> Primary </n-button>
+    <n-button type="info"> Info </n-button>
+    <n-button type="success"> Success </n-button>
+    <n-button type="warning"> Warning </n-button>
+    <n-button type="error"> Error </n-button>
+  </n-space>
+
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
